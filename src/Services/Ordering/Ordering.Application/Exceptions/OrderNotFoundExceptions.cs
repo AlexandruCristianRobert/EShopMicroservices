4a5c7156433f035735ad147472ce16bb1aba8d03
Alexandru Cristian Robert <alexandru.cristian.robert@gmail.com>
2025-02-04 22:51:31 +0200

    Ordering

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/OrderNotFoundExceptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Application.Exceptions
+{
+    public class OrderNotFoundExceptions : NotFoundException
+    {
+        public OrderNotFoundExceptions(Guid id) : base("Order", id)
+        {
+            
+        }
+    }
+}
